b601e72a1f875d6f95f537da7e930c7d4acfe892
Zhenya <jeskov95@gmail.com>
2018-04-08 02:15:26 +0300

    trying db get

--- index.js
+++ index.js
@@ -92,9 +92,9 @@ app.get("/getRems", function (req, res) {
 
     let uri = 'mongodb://<dbuser>:<dbpassword>@ds237669.mlab.com:37669/moc_chatbot_reminderstask_db';
 
-    mongoose.connect(uri);
-
     let db = mongoose.connection;
+
+    mongoose.connect(uri);
 
     db.on('error', console.error.bind(console, 'connection error:'));
 
@@ -125,18 +125,10 @@ app.get("/getRems", function (req, res) {
         let list = [first, second];
 
 
-        Reminder.insertMany(list).then(() => {
+        Reminder.insertMany(list);
 
-            // Only close the connection when your app is terminating
-            return mongoose.connection.close();
-
-        }).catch(err => {
-
-            // Log any errors that are thrown in the Promise chain
-            console.log(err);
-
-        });
-
+        mongoose.connection.close();
+        
 
         let loh = [];
         loh.push({"text": "ty loh"});

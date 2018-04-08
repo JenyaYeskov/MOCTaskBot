ee0f51bec23938a9459c0fdad39531b5148a0bf6
Zhenya <jeskov95@gmail.com>
2018-04-08 04:24:56 +0300

    trying db get

--- index.js
+++ index.js
@@ -90,51 +90,46 @@ app.get("/loh", function (req, res) {
 
 app.get("/getRems", function (req, res) {
 
-    let uri = 'mongodb://<dbuser>:<dbpassword>@ds237669.mlab.com:37669/moc_chatbot_reminderstask_db';
-
+    let uri = 'mongodb://admin:7447030j@ds237669.mlab.com:37669/moc_chatbot_reminderstask_db';
     let db = mongoose.connection;
 
     mongoose.connect(uri);
 
     db.on('error', console.error.bind(console, 'connection error:'));
 
-    db.once('open', function callback() {
-
-        // Create event schema
-        let remSchema = mongoose.Schema({
-            date: String,
-            time: String,
-            event: String
-        });
-
-        // Store event documents in a collection called "songs"
-        let Reminder = mongoose.model('rems', remSchema);
-
-        let first = new Reminder({
-            date: '23.5.18',
-            time: '15.45',
-            event: 'pizdyuli'
-        });
-
-        let second = new Reminder({
-            date: '24.5.18',
-            time: '15.45',
-            event: 'tobi pizda'
-        });
-
-        let list = [first, second];
-
-
-        Reminder.insertMany(list);
-
-        mongoose.connection.close();
-        
-
-        let loh = [];
-        loh.push({"text": "ty loh"});
-        res.send(loh);
+    let remSchema = mongoose.Schema({
+        date: String,
+        time: String,
+        event: String
     });
 
+    let Reminder = mongoose.model('rems', remSchema);
+
+    let first = new Reminder({
+        date: '23.5.18',
+        time: '15.45',
+        event: 'pizdyuli'
+    });
+
+    let second = new Reminder({
+        date: '24.5.18',
+        time: '15.45',
+        event: 'tobi pizda'
+    });
+
+    let list = [first, second];
+
+
+    Reminder.insertMany(list).then(() => {
+
+        return mongoose.connection.close();
+
+    }).catch(err => {
+
+        // Log any errors that are thrown in the Promise chain
+        console.log(err)
+
+    });
 });
 
 // Server index page
--- loh.js
+++ loh.js
@@ -1,10 +1,60 @@
 let express = require('express');
 let app = express();
+const mongoose = require('mongoose');
 
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
-app.get('/*', function(req, res) {
+app.get('/*', function (req, res) {
     let jsonResponse = [];
-    jsonResponse.push({ "text": "Hi. " + (Math.random() * 5 + 1).toFixed(0) + " is a lucky number..." });
+    jsonResponse.push({"text": "Hi. " + (Math.random() * 5 + 1).toFixed(0) + " is a lucky number..."});
     res.send(jsonResponse);
 });
+
+let uri = 'mongodb://admin:7447030j@ds237669.mlab.com:37669/moc_chatbot_reminderstask_db';
+
+let db = mongoose.connection;
+
+mongoose.connect(uri);
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+
+
+    // Create event schema
+    let remSchema = mongoose.Schema({
+        date: String,
+        time: String,
+        event: String
+    });
+
+    // Store event documents in a collection called "songs"
+    let Reminder = mongoose.model('rems', remSchema);
+
+    let first = new Reminder({
+        date: '23.5.18',
+        time: '15.45',
+        event: 'pizdyuli'
+    });
+
+    let second = new Reminder({
+        date: '24.5.18',
+        time: '15.45',
+        event: 'tobi pizda'
+    });
+
+    let list = [first, second];
+
+
+    Reminder.insertMany(list).then(() => {
+
+        return mongoose.connection.close();
+
+    }).catch(err => {
+
+        // Log any errors that are thrown in the Promise chain
+        console.log(err)
+
+    });
+
+
+

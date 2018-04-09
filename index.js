b816c01113cccbd6b4123df54d04b61cafe82244
Zhenya <jeskov95@gmail.com>
2018-04-09 20:30:18 +0300

    improving get and post requests to db

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.get("/getRems", function (req, res) {
     db.once('open', function callback() {
 
 
-        Reminder.find({"messengerId": qwe}).then(docs => {
+        Reminder.find({'messengerId': qwe}).then(docs => {
 
             // docs.forEach(doc =>{
             //

34f10e59a13318af85eab9f5bbffcd231f6d6f8c
Zhenya <jeskov95@gmail.com>
2018-04-10 04:13:43 +0300

    working on delete reminders functionality

--- index.js
+++ index.js
@@ -85,9 +85,15 @@ app.get("/getRems", function (req, res) {
 
         }).then(() => {
 
+            let c = 0;
+
+            Reminder.count({'messengerId': "1898219773585506"}, (err, count) => {
+                c = count + 10;
+            });
+
             if (message.length === 0)
                 res.send("No reminders");
-            else res.send(Reminder.count({'messengerId': "1898219773585506"}));
+            else res.send(c);
 
         }).then(() => {
 

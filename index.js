1c912723c5482a5dafa0b2087d84d62e3b27c33f
Zhenya <jeskov95@gmail.com>
2018-04-27 19:20:04 +0300

    trying to send message from backend

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const
-    // PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN,
-    // request = require('request'),
+    PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN,
+    request = require('request'),
     dotenv = require('dotenv').config(),
     mongoose = require('mongoose'),
     uri = process.env.MONGO_URI,
@@ -430,7 +430,7 @@ function callSendAPI(sender_psid, response) {
     // Send the HTTP request to the Messenger Platform
     request({
         "uri": "https://graph.facebook.com/v2.6/me/messages",
-        "qs": {"access_token": process.env.PAGE_ACCESS_TOKEN},
+        "qs": {"access_token": PAGE_ACCESS_TOKEN},
         "method": "POST",
         "json": request_body
     }, (err, res, body) => {

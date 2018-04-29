d086f218acd44ee2b92514b382b38d25b53febbf
Zhenya <jeskov95@gmail.com>
2018-04-29 23:39:56 +0300

    trying to send message from backend

--- index.js
+++ index.js
@@ -461,7 +461,7 @@ function trySend(mid) {
 
     request({
         "Content-Type": "application/json",
-        "uri": "https://api.chatfuel.com/bots/5ac8230ce4b0336c50287a5d/users/"+mid+"/send?chatfuel_token=" + token + "&chatfuel_block_name=loh",
+        "uri": "https://api.chatfuel.com/bots/5ac8230ce4b0336c50287a5d/users/"+mid+"/send?chatfuel_token=" + token + "&chatfuel_block_id=5ae34ee1e4b088ff003688cf",
         "method": "POST",
         // "json": request_body
     }, (err, res, body) => {

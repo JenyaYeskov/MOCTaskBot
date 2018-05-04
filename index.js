7861a45d03eeedce95c70e07698d5de6308d9ef0
Zhenya <jeskov95@gmail.com>
2018-05-04 21:16:14 +0300

    trying to get id from db

--- index.js
+++ index.js
@@ -225,7 +225,7 @@ app.post("/addRem", (req, res) => {
             console.log(qwe);
 
             let reminderId = qwe["_id"];
-            console.log(reminderId + "  "+ typeof reminderId["$oid"]);
+            console.log(reminderId + "  "+ typeof reminderId["$oid"] + "  "+typeof "jhjk");
 
             schedule.scheduleJob(timeAndDate, (reminderId) =>{
                 fireReminder(reminderId)
